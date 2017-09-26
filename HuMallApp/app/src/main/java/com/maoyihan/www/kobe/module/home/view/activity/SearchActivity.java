0e56e2f2f67a00a790aaf4359c463922e41c02f2
Xmaoyh <353804616@qq.com>
2017-09-26 09:54:59 +0800

    BackIcon

--- HuMallApp/app/src/main/java/com/maoyihan/www/kobe/module/home/view/activity/SearchActivity.java
+++ HuMallApp/app/src/main/java/com/maoyihan/www/kobe/module/home/view/activity/SearchActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.maoyihan.www.kobe.R;
@@ -36,6 +37,8 @@ public class SearchActivity extends BaseBarActivity {
     protected void initView() {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        //后退按钮
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -67,5 +70,12 @@ public class SearchActivity extends BaseBarActivity {
         }
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
